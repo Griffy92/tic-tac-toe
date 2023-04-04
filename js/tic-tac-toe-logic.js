565de5b5a95aa4c8fe5faeccf9866c95a04510a8
Dominic <dominicwong09@gmail.com>
2023-04-04 19:32:59 +1200

    reworked game board & logic. add restart fn & logic

--- js/tic-tac-toe-logic.js
+++ js/tic-tac-toe-logic.js
@@ -1,6 +1,8 @@
+
 let isPlayerXTurn = true;
 
-const nineBoard = {
+const game = {
+    turn: 0,
     player1: {
         name: "X",
         counter: 0,
@@ -9,78 +11,102 @@ const nineBoard = {
         name: "O",
         counter: 0,
     },
-    box1: "",
-    box2: "",
-    box3: "",
-    box4: "",
-    box5: "",
-    box6: "",
-    box7: "",
-    box8: "",
-    box9: "",
-    winner: function () {
-        // omg fucking abomination
-        if (this.box1 + this.box2 + this.box3 === 'ooo' || this.box1 + this.box2 + this.box3 === 'xxx') {
-            return this.box1;
-        } else if ( this.box1 + this.box4 + this.box7 === 'ooo' || this.box1 + this.box2 + this.box3 ===  'xxx' ) {
-            return this.box1;
-        } else if ( this.box1 + this.box5 + this.box9 === 'ooo' || this.box1 + this.box5 + this.box9 ===  'xxx' ) {
-            return this.box1;
-        } else if ( this.box2 + this.box5 + this.box8 === 'ooo' || this.box2 + this.box5 + this.box8 === 'xxx' ) {
-            return this.box2;
-        } else if ( this.box3 + this.box5 + this.box7 === 'ooo' || this.box3 + this.box5 + this.box7 === 'xxx' ) {
-            return this.box3;
-        } else if ( this.box3 + this.box6 + this.box9 === 'ooo' || this.box3 + this.box6 + this.box9 === 'xxx' ) {
-            return this.box3;
-        } else if ( this.box4 + this.box5 + this.box6 === 'ooo' || this.box4 + this.box5 + this.box6 === 'xxx' ) {
-            return this.box4;
-        } else if ( this.box7 + this.box8 + this.box9 === 'ooo' || this.box7 + this.box8 + this.box9 === 'xxx' ) {
-            return this.box7;
+    board: [
+        ["","",""],
+        ["","",""],
+        ["","",""],
+    ],
+    recordMove: function ( row, col ) { 
+        // takes in row col argument from click and updates the game board
+        if ( isPlayerXTurn ) {
+            this.board[row][col] = 'x'
+            isPlayerXTurn = false;
+            this.turn += 1;
+        } else {
+            this.board[row][col] = 'o'
+            isPlayerXTurn = true;
+            this.turn += 1;
         }
+    },
+    checkWin: function ( string ) {
+        // diagnoals
+        if ( this.turn >= 3 ) {
+            if ( this.board[0][0] === string && this.board[1][1] === string && this.board[2][2] === string ) {
+                return true;
+            }
+            if ( this.board[0][2] === string && this.board[1][1] == string && this.board[2][0] === string ) {
+                return true;
+            }
+    
+            for ( let i = 0; i < this.board.length; i ++ ) {
+                if ( this.board[i][0] === string && this.board[i][1] === string && this.board[i][2] === string) { // checks rows
+                    return true;
+                }
+                if ( this.board[0][i] === string && this.board[1][i] === string && this.board[2][i] === string) { // checks columns
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+    checkDraw: function () {
+        if ( this.turn === 9 && this.board.join("") !== ",,,,,,") {
+            alert('draw')
+        }
+    },
+    reset: function () {
+        for ( let i = 0; i < this.board.length; i++ ) {
+            for ( let j = 0; j < this.board[i].length; j++ ) {
+              
+                this.board[i][j] = "";
+            }
+        };
+        game.turn = 0;
+        isPlayerXTurn = true;
     }
 }
 
 const render = function() {
-    $('#box1').text(nineBoard.box1);
-    $('#box2').text(nineBoard.box2);
-    $('#box3').text(nineBoard.box3);
-    $('#box4').text(nineBoard.box4);
-    $('#box5').text(nineBoard.box5);
-    $('#box6').text(nineBoard.box6);
-    $('#box7').text(nineBoard.box7);
-    $('#box8').text(nineBoard.box8);
-    $('#box9').text(nineBoard.box9);
+    $(`#row0-col0`).text(game.board[0][0]);
+    $(`#row0-col1`).text(game.board[0][1]);
+    $(`#row0-col2`).text(game.board[0][2]);
+    $(`#row1-col0`).text(game.board[1][0]);
+    $(`#row1-col1`).text(game.board[1][1]);
+    $(`#row1-col2`).text(game.board[1][2]);
+    $(`#row2-col0`).text(game.board[2][0]);
+    $(`#row2-col1`).text(game.board[2][1]);
+    $(`#row2-col2`).text(game.board[2][2]);
 }
-// update the boxes and checks for winner
 
 $(document).ready( function () {
     
     $('.box').on('click', function(event) {
+        
+        $(this).off('click'); // disables the button after it's been pressed.
+        
+        // get user input on which square was clicked
+        const row = event.target.id.charAt(3); 
+        const col = event.target.id.charAt(8); 
 
-        render();
-        if ( nineBoard[event.target.id] === "" ) {
+        // record player move
+        game.recordMove(row, col);
 
-            if ( isPlayerXTurn ) {
-                nineBoard[event.target.id] = 'x'
-                isPlayerXTurn = false;
-                winner = nineBoard.winner();
-                if (winner) {
-                    console.log('Player X is the winner')
-                }
-            } else {
-                nineBoard[event.target.id] = 'o'
-                isPlayerXTurn = true;
-                winner = nineBoard.winner();
-                if (winner) {
-                    console.log('Player O is the winner')
-                }
-            }
-        }
+        if ( game.checkWin('x') ) {
+            alert('x wins!');
+        };
+        if ( game.checkWin('o') ) {
+            alert('o wins')
+        };
+        game.checkDraw();
 
-        // click a box and put a marker on it
         // check winner
-        render();
+        render(); // update the game board
     })
 
-});
+    $('#restart').on('click', function () {
+        game.reset();
+        render();
+        $('.box').on('click');
+    });
 
+});
